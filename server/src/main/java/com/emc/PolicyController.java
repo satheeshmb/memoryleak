326db44cb595e489e8922bd48f873f48fe2946af
Mike Schmidt <michael.schmidt@emc.com>
2016-08-29 12:36:02 -0600

    Policy Changes

--- server/src/main/java/com/emc/PolicyController.java
+++ server/src/main/java/com/emc/PolicyController.java
@@ -1,6 +1,8 @@
 package com.emc;
 
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +13,7 @@ public class PolicyController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    @CrossOrigin(origins = "*") 
     @RequestMapping("/policy")
     public Policy greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Policy(counter.incrementAndGet(),
