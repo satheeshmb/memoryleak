b391d0d6855109766c6917d499e2056c002e07e0
Brian Freeman <brian@boldertechnologies.com>
2016-08-29 11:21:48 -0600

    Update policy & client

---
+++ server/src/main/java/com/emc/Client.java
@@ -0,0 +1,12 @@
+package com.emc;
+
+import org.springframework.data.annotation.Id;
+
+/**
+ * Created by brian on 8/29/16.
+ */
+public class Client {
+
+    @Id
+    private String id;
+}
--- server/src/main/java/com/emc/Policy.java
+++ server/src/main/java/com/emc/Policy.java
@@ -1,23 +1,15 @@
-package hello.model;
+package com.emc;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.Version;
-import org.springframework.data.mongodb.core.index.Indexed;
-import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
 
-@Document
 public class Policy {
 
     @Id
     private String id;
 
-    @Version
-    private Long version;
-
-    /** policy names need to be unique */
-    @Indexed(unique = true)
     private String policyName;
+
+    private List<Client> clients;
 }
